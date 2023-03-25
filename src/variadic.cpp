1b5839f6ce1532575322665b46443781d90ba8f7
pemathins <pemathinleyrong18@gmail.com>
2023-03-26 00:09:14 +0530

    variadic Templates

--- src/variadic.cpp
+++ src/variadic.cpp
@@ -4,16 +4,18 @@ struct Test
 {
 private:
     int i;
+    template<typename... T,typename N>
+    void printValue(T... t) { std::cout << std::get<N>(t...) <<"\n";}
 public:
     template<typename ... T>
-    Test(T&& ... x) 
-        :i(std::move(x))
+    Test(T&& ... x)
     {
+        printValue(x...);
     }
     int fptr = [](int i){ std::cout << i*i; return i*i; }(i);
     
 };
 
 int main() {
-    Test{32};
+    Test{32 ,56 ,23.2f};
 }
