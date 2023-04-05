e8c6496b2287aa22e30750ae8ae92a645042cf50
pemathins <pemathinleyrong18@gmail.com>
2023-04-05 22:45:23 +0530

    test0.001.001b.01 userinfo registeration

--- src/testproject01/n1.cpp
+++ src/testproject01/n1.cpp
@@ -1,7 +1,55 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
+namespace access
+{
+    static int count=0;
+    static int16_t TOKEN=0;
+}
+typedef struct UserInfo
+
+{
+    std::string first_name;
+    std::string middle_name;
+    std::string last_name;
+    std::string e_mail;
+    std::string date_of_Birth;
+    UserInfo(std::string& fname, std::string& mname, std::string& lname, std::string& email, std::string& dob) 
+        :first_name(fname), middle_name(mname), last_name(lname), e_mail(email) ,date_of_Birth(dob)
+    {
+    }
+
+};
+
+namespace person 
+{
+    class Register
+    {
+        public:
+        void person(std::vector<UserInfo>& people)
+        {
+            std::string fname,mname,lname,email,dob;
+            do
+            {
+                std::cout << "\nEnter your first name : ";
+
+                std::cin >> fname >> mname >> lname >> email >> dob;
+                std::cout << "\nEnter your middle name : ";
+                std::cin >> mname;
+                std::cout << "\nEnter your last name : ";
+                std::cin >> lname;
+                std::cout << "\nEnter your email : ";
+                std::cin >> email;
+                std::cout << "\nEnter your date of birth : ";
+                std::cin >> dob;
+                UserInfo person{fname,mname,lname,email,dob};
+
+            } while(true);
+        }
+    };
+}
 int main() 
 {
     int a,n =0;
@@ -10,12 +58,27 @@ int main()
     std::cout << "_______________________________________________\n";
     std::cout << "|            1. Register                      |\n";
     std::cout << "|            2. Login                         |\n";
-    std::cout << "|            1. Cnhange Password              |\n";
-    std::cout << "|            1. End Program                   |\n";
+    std::cout << "|            3. Cnhange Password              |\n";
+    std::cout << "|            4. End Program                   |\n";
     std::cout << "|_____________________________________________|\n";
 
+
+    std::vector<UserInfo> user;
+    person::Register person[count];
+    person.
     do {
         std::cout << "Enter your choice :-";
         std::cin >> a;
+        switch(a)
+        {
+            case 1:
+            user.insert(person[count]); 
+                access::TOKEN += 1;
+                std::cout << "_____________________________________________\n";
+                std::cout << "|                                            |\n";
+                std::cout << "|-----------------register-------------------|\n";
+                std::cout << "|____________________________________________|\n";
+
+        }
     }while(a!=4); 
 }
