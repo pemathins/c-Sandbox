ef06ca905b18828efda685efd0d583d9415ed375
pemathins <pemathinleyrong18@gmail.com>
2023-04-08 22:20:49 +0530

    test3

--- src/testproject01/client.cpp
+++ src/testproject01/client.cpp
@@ -1,23 +1,49 @@
 #include <iostream>
 #include <string>
 
-void cipher(int mode,std::string&& plaintext, int key)
+void cipher(bool mode,std::string&& plaintext, int key)
 {
     const char* array = plaintext.c_str();
     size_t x = sizeof(array)/2;
 
     for(int i=0;i<(int)x;i++){
         // first to mod the value like -> x mod 26
-        int n = (int)array[i];
-        float y = (n+key)/26;
+        float n;
+        if((int)array[i]>=97&& (int)array[i]<124){
+            n = (int)array[i]-96;
+            std::cout << "value of n: " << n << " value of array if i : "<< array[i]<<","<< (int)array[i]-96 << std::endl;
+        }
+        
+        if((int)array[i]>=65 && (int)array[i]<91)
+        n = (int)array[i]-64;
+        
+        double y;
+        if(mode == true)
+        {
+            y = (n+key)/26;
+        }
+        else
+        {
+            y = (n-key)/26;
+        }
 
         int a  = (y-(int)y)*26;
-        std::cout << "foat : " << y << " " << (int)array[i] << (int)a << ",";
+
+        if((int)array[i]>=97&& (int)array[i]<124)
+        {
+            std::cout << (char)(a+96) << " "<< "\n";
+        }
+        if((int)array[i]>=65 && (int)array[i]<91)
+        {
+            std::cout << (char)(a+64) << " "<< "\n";
+        }
+        
 
     }
 }
 
 int main()
 {
-    cipher(1,"neso",2);
+    std::cout << (int)'a' << std::endl;
+    cipher(false,"bB",2);
 }
