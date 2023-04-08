d08816354b9789cebeb96d0e07f3cd4a03019103
pemathins <pemathinleyrong18@gmail.com>
2023-04-08 20:03:01 +0530

    test2

--- src/testproject01/client.cpp
+++ src/testproject01/client.cpp
@@ -4,12 +4,20 @@
 void cipher(int mode,std::string&& plaintext, int key)
 {
     const char* array = plaintext.c_str();
-    for(int i=0;i<(int)sizeof(array);i++){
-        std::cout << (int)array[i] << ",";
+    size_t x = sizeof(array)/2;
+
+    for(int i=0;i<(int)x;i++){
+        // first to mod the value like -> x mod 26
+        int n = (int)array[i];
+        float y = (n+key)/26;
+
+        int a  = (y-(int)y)*26;
+        std::cout << "foat : " << y << " " << (int)array[i] << (int)a << ",";
+
     }
 }
 
 int main()
 {
-    cipher(1,"Pema",2);
+    cipher(1,"neso",2);
 }
