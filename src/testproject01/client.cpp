e63b3677a43d6295b9bc1dc6361b059f2ffa5cd3
pemathins <pemathinleyrong18@gmail.com>
2023-04-08 19:37:07 +0530

    test1

--- src/testproject01/client.cpp
+++ src/testproject01/client.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <string>
+
+void cipher(int mode,std::string&& plaintext, int key)
+{
+    const char* array = plaintext.c_str();
+    for(int i=0;i<(int)sizeof(array);i++){
+        std::cout << (int)array[i] << ",";
+    }
+}
 
 int main()
 {
-    std::cout << "client";
+    cipher(1,"Pema",2);
 }
