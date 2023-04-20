dc594d72d8ba1b8d626a7ab54f520a7434489b90
pemathins <pemathinleyrong18@gmail.com>
2023-04-21 00:28:46 +0530

    test1

--- DataStructures/linklist.cpp
+++ DataStructures/linklist.cpp
@@ -9,14 +9,38 @@ namespace ca
         struct node
         {
             T Value;
-            node * Nextadd;
+            node* Nextadd;
         };
         
-        mutable int counter=0;
-        node * startNode;
-        node * first;
-        node * last;
-        int capicity() const
+        mutable int counter=0,traverserCounter=0,counterSet=0;
+        node* startNode;
+        node* first;
+        node* currentNode = NULL;
+
+        node* nodeTraverser(node* nodePointer,int m_counter)
+        {
+            node* traverse = nodePointer;
+            std::cout << traverse->Value << "\n";
+            if(traverse == first)
+                return 0;
+            if(traverse->Nextadd == (std::nullptr_t)nullptr)
+                return traverse;
+            counterSet++;
+            nodeTraverser(traverse->Nextadd,counterSet);
+        }
+
+        int nodeTraverserPrint(node* nodePointer,int m_count)
+        {
+            if(m_count == counterSet)
+                return 0;
+            node* traverse = nodePointer;
+            std::cout << traverse->Value << "\n";
+            
+            counterSet++;
+            nodeTraverserPrint(traverse->Nextadd,m_count);
+        }
+
+        int size() const
         {
             return counter;
         }
@@ -29,23 +53,25 @@ namespace ca
         bool add(const T& V)
         {
             first = (struct node*)malloc(1*sizeof(struct node));
-            last = (struct node*)malloc(1*sizeof(struct node));
-            first->Value = V;
-            first->Nextadd = last;
             if(counter == 0)
                 startNode = first;
+
+            first->Value = V;
+            first->Nextadd = nodeTraverser(startNode,counter);
             counter++;
+            currentNode = first;
+            std::cout << "my Address is : " << first << " | counter : " << counter << "\n";
+            std::cout << "value : " << first->Value << " | nextaddress : " << first->Nextadd << "\n";
             return true;
         }
-        T item(int const& index)
+
+        void item(int const& index)
         {
-            while(!(index-1))
-            {
-                node * ptr = first->Nextadd;
-                first = last;
-            }
-            return first->Value;
+            // node* current = nodeTraverser(startNode,index);
+            nodeTraverserPrint(startNode,index);
+            counterSet=0;
         }
+
         ~linklist()
         {
             /*free the struct
@@ -59,13 +85,17 @@ namespace ca
 int main() 
 {
     ca::linklist<int> newlist;
-    newlist.add(45);
-    newlist.add(43);
     newlist.add(41);
     newlist.add(42);
-    for(int i=0;i<4;i++)
-    {
-        std::cout << newlist.item(i);
-    }
+    newlist.add(43);
+    newlist.add(44);
+    newlist.add(45);
+    newlist.add(46);
+    
+    newlist.item(5);
+    // for(int i=0;i<4;i++)
+    // {
+    //     std::cout << newlist.item(i);
+    // }
       
 }
