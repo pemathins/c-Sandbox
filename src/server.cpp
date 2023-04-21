65db7aa2d61b787d10577433ce5f7ab4058f4255
pemathins <pemathinleyrong18@gmail.com>
2023-04-21 13:25:53 +0530

    test2

--- src/server.cpp
+++ src/server.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <sys/types.h>
+#include <inttypes.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+
 
 int main()
 {
+    int connect(int socket_descriptor, const struct sockaddr *address, socklen_t length_of_address);
+
     
 }
