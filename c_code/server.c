4a9c2e3046136a387935d47138f5ac83e3a929ce
pemathins <pemathinleyrong18@gmail.com>
2023-04-26 21:58:07 +0530

    test1

--- c_code/server.c
+++ c_code/server.c
@@ -7,10 +7,11 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <inttypes.h>
 #include "pemasheader.h"
 
 #define PORT 8080
-int main(int argc, char const* argv[])
+int main()
 {
 	int server_fd, new_socket, valread;
 	struct sockaddr_in address;
@@ -20,40 +21,21 @@ int main(int argc, char const* argv[])
 	char* hello = "Hello from server";
 
 	// Creating socket file descriptor
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		perror("socket failed");
-		exit(EXIT_FAILURE);
-	}
+	int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-	// Forcefully attaching socket to the port 8080
-	if (setsockopt(server_fd, SOL_SOCKET,
-				SO_REUSEADDR | SO_REUSEPORT, &opt,
-				sizeof(opt))) {
-		perror("setsockopt");
-		exit(EXIT_FAILURE);
-	}
+    int setsockopt(serverSocket, IPPROTO_TCP, SO_DEBUG, const void *value_of_option, socklen_t option_length);
+
+	
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = INADDR_ANY;
 	address.sin_port = htons(PORT);
 
-	// Forcefully attaching socket to the port 8080
-	if (bind(server_fd, (struct sockaddr*)&address,
-			sizeof(address))
-		< 0) {
-		perror("bind failed");
-		exit(EXIT_FAILURE);
-	}
-	if (listen(server_fd, 3) < 0) {
-		perror("listen");
-		exit(EXIT_FAILURE);
-	}
-	if ((new_socket
-		= accept(server_fd, (struct sockaddr*)&address,(socklen_t*)&addrlen))
-		< 0) {
-		perror("accept");
-		exit(EXIT_FAILURE);
-	}
-	valread = read(new_socket, buffer, 1024);
+	bind(server_fd, (struct sockaddr*)&address,sizeof(address));
+	
+	
+	accept(server_fd, (struct sockaddr*)&address,(socklen_t*)&addrlen);
+	
+	read(new_socket, buffer, 1024);
 
  
     int len = strlen(buffer)+1;
@@ -71,7 +53,7 @@ int main(int argc, char const* argv[])
 	// send respond
 	char notpalandromic[] = "String is Not palandromic.\0";
 	char isPalandromic[] = "String is palandromic.\0";
-    if(palandromic(buffer,rev)==0)
+    if(strcmp(buffer,rev)==0)
 		send(new_socket, isPalandromic, strlen(isPalandromic), 0);
     else
 		send(new_socket, notpalandromic, strlen(notpalandromic), 0);
