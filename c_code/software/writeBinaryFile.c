2e270dbfe24c5709ae62e31074beb7cfbfe06db9
pemathins <pemathinleyrong18@gmail.com>
2023-05-01 12:21:45 +0530

    test4

---
+++ c_code/software/writeBinaryFile.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
+
+//#define const char* FILE_FORMAT = "%d,%s\n"
+
+typedef struct User
+{
+    int id;
+    char name[35];
+    
+} User;
+
+int main()
+{
+    User pema = {
+        .id=8,
+        .name="pema"
+    },user2;
+
+    char buffer_out[50];    
+    FILE* out;
+
+    out = fopen("textbin.bin", "wb");
+
+    if(out==0)
+    {
+        printf("File opening Error\n");
+        return 1;
+    }
+
+    printf("%d",sizeof(pema));
+    
+    if(fwrite(&pema,sizeof(User),1,out)==0)
+    {
+        fclose(out);
+        return 2;
+    }
+    fclose(out);
+
+
+    
+
+    return 0;
+} 
