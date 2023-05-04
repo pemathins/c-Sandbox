f2adc4bcd32bde830f6c70d7ba50d499928656b2
pemathins <pemathinleyrong18@gmail.com>
2023-05-05 00:34:30 +0530

    test100

--- c_code/software/readFile.c
+++ c_code/software/readFile.c
@@ -9,29 +9,47 @@ double cpu_time_used;
 typedef struct User
 {
     int id;
-    char* name[35];
+    char name[35];
 } User;
 
 int main()
 {
-    start = clock();
-    User user;
-    char buffer_in[50];
-    FILE* in;
+    // start = clock();
+    // User user;
+    // char buffer_in[50];
+    // FILE* in;
 
-    in = fopen("text.dat", "r");
+    // in = fopen("text.dat", "r");
 
-        if(fgets(buffer_in, sizeof buffer_in,in)==0)
-        {
-            fclose(in);
-            return 1;
-        }
-    fclose(in);
+    //     if(fgets(buffer_in, sizeof buffer_in,in)==0)
+    //     {
+    //         fclose(in);
+    //         return 1;
+    //     }
+    // fclose(in);
 
-    sscanf(buffer_in,"%d,%[^\n]",&user.id,user.name);
-    printf("name = %s, id = %d",user.name,user.id);
-    end = clock();
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    printf("\nTime Taken to read file : %0.8f",cpu_time_used);
+    // sscanf(buffer_in,"%d,%[^\n]",&user.id,user.name);
+    // printf("name = %s, id = %d",user.name,user.id);
+    // end = clock();
+    // cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    // printf("\nTime Taken to read file : %0.8f",cpu_time_used);
+    FILE* file = fopen("./Login_Page/fonts/font-awesome-4.7.0/css/font-awesome.min.css","r");
+
+    if(file==0) return 1;
+	fseek(file,0,SEEK_END);
+	int length = ftell(file);
+	fseek(file, 0, SEEK_SET);
+
+	char* buffer = (char*)malloc(sizeof(char)*(length));
+
+	char c;
+	int i =0;
+	while((c=fgetc(file))!= EOF)
+	{
+		buffer[i]=c;
+		i++; 
+	}
+	fclose(file);
+	printf(" file to send ++++++++++++++++++++++++++++++++++++++++++++++\n\n%s\ni:%d",buffer,i);
     return 0;
 }
--- c_code/software/server.c
+++ c_code/software/server.c
@@ -9,9 +9,20 @@
 
 #define PORT 18000
 #define SA struct sockaddr*
-#define BUFFER_MAX_SIZE_IN 4096
+#define BUFFER_MAX_SIZE_IN 409600
+#define GET "GET "
+#define POST "POST"
+#define ERR "X0"
+
+typedef struct User
+{
+	int id;
+	char name[35];
+} User;
 
 char* webPageLoader(FILE*);
+
+char* request(char*);
 
 int main(int argc, char* argv[])
 {
@@ -21,13 +32,14 @@ int main(int argc, char* argv[])
 	int addrlen = sizeof(address);
 	char buffer_in_get[BUFFER_MAX_SIZE_IN] = { 0 };
 	char buffer_final[BUFFER_MAX_SIZE_IN];
-
+	char bufferTest[55];
 	
 	//load the web page
-	FILE* file = fopen("login.html", "r");
+	FILE* authFile = fopen("./Login_Page/login.html", "r");
+	FILE* loginDashboardFile = fopen("Dashboard.html", "r");
 
 	// returns a heap allocated buffer so please free it the escape memory leaks
-	char* bufferpage = webPageLoader(file);
+	char* bufferpage = webPageLoader(authFile);
 
 	sprintf(buffer_final,HEADER_W_CONTENT,strlen(bufferpage),bufferpage);
 	printf("\n%s",buffer_final);
@@ -41,20 +53,38 @@ int main(int argc, char* argv[])
 	bind(server_fd, (SA)&address, sizeof(address));
 
 	listen(server_fd, 1);
-	for(;;)
+	for(int i=0;;i++)
 	{
 
+		char* sendfile,fileDir[100]="./Login_Page";
 		memset(buffer_in_get,0,BUFFER_MAX_SIZE_IN);
+
 		
 		new_socket = accept(server_fd, NULL, NULL);
 
 		// get the request from the client 
 		valread = read(new_socket, buffer_in_get, BUFFER_MAX_SIZE_IN);
 
-		fprintf(stdout,"\n%s\n",buffer_in_get);
+		fprintf(stdout,"Buffer_IN_GET after free :\n%s\n\ni count : %d\n\n",buffer_in_get,i);
+
+		
 
 		// Send the web page to the user 
-		valwrite = write(new_socket, buffer_final,strlen(buffer_final));
+		if(i==0)
+			valwrite = write(new_socket, buffer_final,strlen(buffer_final));
+		if(i>0)
+		{
+			if(strcmp(request(buffer_in_get),GET)==0)
+			{
+				sscanf(buffer_in_get,"%*c%*c%*c%*c%100[^ ]",fileDir+12);
+				printf("\nfilename riquested : |%s\n", fileDir);
+				FILE *SenderPage = fopen(fileDir,"rb");
+				sendfile = webPageLoader(SenderPage);
+
+			}
+			valwrite = write(new_socket, sendfile,strlen(sendfile));
+		}
+			
 
 		close(new_socket);
 	}
@@ -81,6 +111,27 @@ char* webPageLoader(FILE* file)
 		i++; 
 	}
 	fclose(file);
-	printf("%s",buffer);
+	printf("\nfile to sent [========================================]100\n\n%s\n",buffer);
+	return buffer;
+}
+
+
+char* request(char* buffer)
+{
+	char temp[4];
+	strncpy(temp, buffer,4);
+	if(strcmp(temp,GET))
+	return GET;
+	if(strcmp(temp,POST))
+	return POST;
+	return ERR;
+
+}
+
+
+char* loginChecker(char* getRequest)
+{
+	
+	char* buffer;
 	return buffer;
 }
