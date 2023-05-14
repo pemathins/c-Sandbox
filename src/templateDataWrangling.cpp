e9ea028c87ada0368b26f5a3b1d3cf53770ff041
pemathins <pemathinleyrong18@gmail.com>
2023-05-14 22:19:03 +0530

    test5

--- src/templateDataWrangling.cpp
+++ src/templateDataWrangling.cpp
@@ -1,3 +1,33 @@
 #include <iostream>
-#include <climits>
+#include <cstdio>
+#include <vector>
 
+typedef struct User
+{
+    int id;
+    std::string user_name_first;
+    std::string user_name_last;
+    std::string user_email;
+    std::string user_gender;
+    std::string user_dob;
+
+    User(int id, std::string& fname, std::string& lname, std::string& email, std::string& gender, std::string& dob) :user_name_first(fname), user_name_last(lname), user_email(email), user_gender(gender), user_dob(dob)
+    {}
+    User()
+    {}
+    
+} User;
+
+int main()
+{
+    User temp;
+    FILE* data = fopen("testSet.csv", "r");
+    if(data ==0)
+    {
+        std::cerr << "no Data!!\n";
+        return 1;
+    }
+    std::vector<User> user;
+    fscanf(data,"%d,%s,%s,%s,%s,%s",temp.id,temp.user_name_first,temp.user_name_last,temp.user_email,temp.user_gender,temp.user_dob);
+    user.emplace_back(temp);
+}
