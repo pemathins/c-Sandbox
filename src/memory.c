098d4ef1ab8694f40c9706462d905fdafc127d92
pemathins <pemathinleyrong18@gmail.com>
2023-06-08 11:09:59 +0530

    function created char pointer to int pointer array

--- src/memory.c
+++ src/memory.c
@@ -3,35 +3,59 @@
 #include<string.h>  
 
 #define charptr char*
+#define intptr int*
+
+int* intBuffer(char*, char,int*,int*);
 int main()
 {
-    char splitAt = 32;
-    size_t count =0;
+    char buffer[50];
+    int* arrlen=(int*)malloc(sizeof(int));
+    printf("Enter your arr elements : ");
+    scanf("%[^\n]",buffer);
+    int* arr = (int*)malloc(50*sizeof(int));
+    arr = intBuffer(buffer,' ',arr,arrlen);
+    // print the int array
+    for(int j=0;j<*arrlen;j++)
+        printf("%d) -> %d\n",j,arr[j]);
+    free(arr);
+    free(arrlen);
+}
+
+//it takes a buffer (string value) and conversts int* arrays 
+//parameters buffer string vaue,splitAt char to split at, array an stack allocated array of int pointers and arrlen the length of the array also stack int pointer 
+int* intBuffer(char* buffer, char splitAt,int* array,int* arrlen)
+{
+    // char splitAt = 32;
+    size_t count =0,t=0;
     charptr block[10];    
     charptr rx;
-
-    void* temp = malloc(5*sizeof(int));
-
+    void* temp = malloc(strlen(buffer)*sizeof(char));
     char* address = (char*)temp;
     rx = address;
-    const char* name = "34 35 56 23 67 89";
+    // const char* buffer,name = "34 35 56 23 67 89";
 
-    memcpy(temp,name,strlen(name));
-    for(size_t i=0;i<strlen(name);i++)
+    memcpy(temp,buffer,strlen(buffer));
+    //printf("Buffer is : %s",buffer);
+    for(size_t i=0;i<strlen(buffer);i++)
     {
         char* r = (char*)(temp+i);
         char x = (char)*r;
-        if(x==splitAt || i==strlen(name)-1)
+        if(x==splitAt || i==strlen(buffer)-1)
         {
             memcpy(block,rx,count);
             count = 0;
             rx = (char*)(temp+i+1);
+            array[t] = atoi((const char*)block);
+            t++;
 
         }
-        printf("Bock : %s\n",block);
+        //printf("Bock : %s\n",block);
         memset(block,0,10);
+        
         count++;
-
-        printf("%c",x); 
+        //printf("%c",x); 
     }
+    *arrlen = t;
+    free(temp);
+    return array;
 }
